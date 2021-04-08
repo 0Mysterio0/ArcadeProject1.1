db9309b74a8fe44601ad5911a616867b088f1844
Erin Carrier <carrieer@gvsu.edu>
2021-04-08 14:37:45 -0400

    a random comment to check git setup

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 #hello
+# test comment to work with Tess's git setupd
 
 def print_hi(name):
     ## Use a breakpoint in the code line below to debug your script.
