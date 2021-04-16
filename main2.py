56fba75994b3155615f116f0299a2026639281b7
Seth <seth.woodwyk@gmail.com>
2021-04-16 12:37:30 -0400

    Added inital movement, tested out boundaries

--- main2.py
+++ main2.py
@@ -30,7 +30,13 @@ CHARACTER_SCALING = 1
 TILE_SCALING = 0.5
 COIN_SCALING = 0.5
 
-
+# Movement speed of player, in pixels per frame
+PLAYER_MOVEMENT_SPEED = 10
+GRAVITY = 1
+PLAYER_START_X = 64
+PLAYER_START_Y = 128
+msg="Here we define our constants for our game,such as how fast our player will move across the screen{0}".format(PLAYER_MOVEMENT_SPEED)
+print(msg)
 class MyGame(arcade.Window):
     """
     Main application class.
@@ -46,6 +52,7 @@ class MyGame(arcade.Window):
         self.coin_list = None
         self.wall_list = None
         self.player_list = None
+        self.fruit_list= None
 
         # Separate variable that holds the player sprite
         self.player_sprite = None
@@ -58,12 +65,13 @@ class MyGame(arcade.Window):
         self.player_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList(use_spatial_hash=True)
         self.coin_list = arcade.SpriteList(use_spatial_hash=True)
+        self.fruit_list = arcade.SpriteList()
 
         # Set up the player, specifically placing it at these coordinates.
         image_source = ":resources:images/animated_characters/female_adventurer/femaleAdventurer_idle.png"
         self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
-        self.player_sprite.center_x = 64
-        self.player_sprite.center_y = 128
+        self.player_sprite.center_x = PLAYER_START_X
+        self.player_sprite.center_y = PLAYER_START_Y
         self.player_list.append(self.player_sprite)
 
         # Create the ground
@@ -76,12 +84,14 @@ class MyGame(arcade.Window):
 
         # Put some crates on the ground
         # This shows using a coordinate list to place sprites
-        coordinate_list = [[512, 96],
-                           [256, 96],
-                           [768, 96]]
-
+        coordinate_list = [[-30, 96],
+                           [1000, 96]]
+        # Create the 'physics engine'
+        #The first
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.wall_list)
+        self.physics_engine=arcade.PhysicsEnginePlatformer(self.player_sprite, self.wall_list)
         for coordinate in coordinate_list:
-            # Add a crate on the ground
+            #Add a crate on the ground
             wall = arcade.Sprite(":resources:images/tiles/boxCrate_double.png", TILE_SCALING)
             wall.position = coordinate
             self.wall_list.append(wall)
@@ -97,6 +107,26 @@ class MyGame(arcade.Window):
         self.coin_list.draw()
         self.player_list.draw()
 
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+        if key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        if key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        if key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+
+    def on_update(self, delta_time):
+        """ Movement and game logic """
+
+        # Move the player with the physics engine
+        self.physics_engine.update()
 
 def main():
     """ Main method """
